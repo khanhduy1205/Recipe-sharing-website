17f4aeb284bee94fb0915035eb3b2156620512c1
KhanhDuy <bkduy001@gmail.com>
2023-09-22 14:30:41 +0700

    push

---
+++ models/carts.m.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+const pathDb = './db/cart.json';
+
+module.exports = {
+    getAll: () => {
+        return new Promise((resolve, reject) => {
+            fs.readFile(pathDb, (err, data) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(JSON.parse(data));
+                }
+            });
+        });
+    },
+};
